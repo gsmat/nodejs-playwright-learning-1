2442c400a5827a8a0404fcc2b60a927320ea542b
Gismat <qismeths@gmail.com>
2022-03-23 01:37:03 +0400

    Catch Api request and feth data

--- skyscannerParse.js
+++ skyscannerParse.js
@@ -1,35 +1,109 @@
-const {chromium} = require('playwright')
+const {firefox} = require('playwright');
 
-async function main() {
-    const browser = await chromium.launch({
-        headless: false,
-        devtools: true,
+/*
+bilet uzerindeki butun melumatlar
+teyyare kodlari ve aid oldugu sirketler
+
+stop_ids => [legs] equal [places].id
+
+*/
+
+
+const url = 'https://www.skyscanner.net/';
+
+(async () => {
+
+    const browser = await firefox.launch({
+        headless: false
     })
-    const context = await browser.newContext();
+
+    const context = await browser.newContext()
 
     const page = await context.newPage()
 
-    await page.goto('https://www.skyscanner.net/transport/flights/gyd/ista/220323/220330/?adults=2&adultsv2=2&c=&cabinclass=economy')
+    await page.goto(url)
 
-    await page.click('text=Show more results')
-    await page.waitForFunction(() => {
-        const priceDivs = document.querySelectorAll('div.FlightsResults_dayViewItems__ZDFlO div')
-        return priceDivs.length > 20
-    })
-    const prices = await page.$$eval('div.FlightsResults_dayViewItems__ZDFlO div', (priceDivs) => {
-        return priceDivs.map(priceDiv => {
-            const start = priceDiv.querySelectorAll('div.LegInfo_routePartialDepart__NzEwY > span.BpkText_bpk-text__YWQwM ')
-            const end = priceDiv.querySelectorAll('div.LegInfo_routePartialArrive__Y2U1N > span.BpkText_bpk-text__YWQwM ')
-            return {
-                start: (start),
-                end: (end)
+    await fillInputs(page)
+
+    page.locator('.BpkButtonBase_bpk-button__NTM4Y').click();
+
+    await page.waitForNavigation();
+
+    const data = await callApiData(page)
+
+    const itineraries = data['itineraries']
+    const segments = data['segments']
+
+    let segmentsArr = []
+    for await (let key of Object.keys(segments)){
+        segmentsArr={
+            id: segments[key].id,
+            origin_place_id: segments[key].origin_place_id,
+            destination_place_id: segments[key].destination_place_id,
+            arrival: segments[key].arrival,
+            departure: segments[key].departure,
+            duration: segments[key].duration,
+            marketing_flight_number: segments[key].marketing_flight_number,
+            marketing_carrier_id:segments[key].marketing_carrier_id,
+            operating_carrier_id: segments[key].operating_carrier_id,
+            mode: segments[key].mode
+        }
+    }
+
+
+    let prices = []
+    for await (let key of Object.keys(itineraries)) {
+        const pricingOptions = itineraries[key]['pricing_options'];
+
+        let options = [];
+        for await (let index of Object.keys(pricingOptions)) {
+            options = {
+                agentIds: pricingOptions[index]['agent_ids'],
+                mainPrice: pricingOptions[index]['price']['amount']
             }
-        })
-    })
 
-    // console.log(prices)
+            let itemsArr = [];
+            const items = pricingOptions[index]['items'];
+            for await (let key of Object.keys(items)) {
+                itemsArr = {
+                    itemAgentId: items[key]['agent_id'],
+                    itemPrice: items[key]['price']['amount'],
+                }
+            }
+            options['item'] = {
+                itemsArr
+            }
+        }
+        prices[key] = {
+            id: itineraries[key].id,
+            option: options
+        }
+    }
+    console.dir(prices)
+
+
     browser.close();
 
+
+})();
+
+const keywords = {
+    start: "Baku",
+    end: "Istanbul (Any)"
 }
 
-main()
+async function fillInputs(page) {
+    await page.click('#fsc-trip-type-selector-one-way');
+    await page.click('#fsc-origin-search');
+    await page.fill('#fsc-origin-search', keywords.start)
+
+    await page.click('#fsc-destination-search')
+    await page.fill('#fsc-destination-search', keywords.end)
+}
+
+async function callApiData(page) {
+    const [response] = await Promise.all([
+        page.waitForResponse('**/g/conductor/v1/**'),
+    ]);
+    return response.json();
+}
